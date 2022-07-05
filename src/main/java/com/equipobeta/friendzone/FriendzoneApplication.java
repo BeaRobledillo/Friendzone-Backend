62dab9f18242f6f8f34a1ca76ecd607417922180
Anitameri <la_petite_de_la_famille@hotmail.com>
2022-07-05 11:35:19 +0200

    first commit

---
+++ src/main/java/com/equipobeta/friendzone/FriendzoneApplication.java
@@ -0,0 +1,13 @@
+package com.equipobeta.friendzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendzoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendzoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/equipobeta/friendzone/FriendzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipobeta.friendzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendzoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

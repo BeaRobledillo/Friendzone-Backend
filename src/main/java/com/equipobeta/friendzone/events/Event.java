92b65b949ec8292f6fe669c79ce6530c69d70658
jesusod <jesusoterodevesa@gmail.com>
2022-07-12 14:30:51 +0200

    aplicattion properties modified

---
+++ src/main/java/com/equipobeta/friendzone/events/Event.java
@@ -0,0 +1,18 @@
+package com.equipobeta.friendzone.events;
+
+
+import javax.persistence.Entity;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+
+
+public class Event implements Serializable{
+
+    @Serial
+    private static final Long serialVersionUID = 1L;
+
+
+
+}

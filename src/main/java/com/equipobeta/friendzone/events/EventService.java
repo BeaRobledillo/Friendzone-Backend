8e5229b0b30541129f4301703d2e9704be4ddcad
jesusod <jesusoterodevesa@gmail.com>
2022-07-13 14:30:34 +0200

    list all events backend finished

---
+++ src/main/java/com/equipobeta/friendzone/events/EventService.java
@@ -0,0 +1,20 @@
+package com.equipobeta.friendzone.events;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EventService {
+
+    private final EventRepository eventRepository;
+
+    public EventService(EventRepository eventRepository){
+        this.eventRepository=eventRepository;
+    }
+
+    public List<Event> getAllEvents(){
+        return eventRepository.findAll();
+    }
+
+}
---
+++ src/main/java/com/equipobeta/friendzone/users/User.java
@@ -0,0 +1,58 @@
+package com.equipobeta.friendzone.users;
+
+import com.equipobeta.friendzone.events.Event;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Collection;
+
+@Entity
+
+@Getter
+@Setter
+
+public class User implements Serializable {
+    @Serial
+    private static final Long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String username;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "event_users",
+            joinColumns = @JoinColumn(
+                    name = "user_id"),
+            inverseJoinColumns = @JoinColumn(
+                    name = "event_id"))
+    private Collection <Event> events;
+
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String password, String username) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.username = username;
+    }
+
+    public User(String name, String email, String password, String username) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.username = username;
+    }
+
+
+}
